f8dcfe981f9aa4562c6f08b0d74ae6fa40cda7f1
Abylay03 <bite2099@gmail.com>
2021-10-29 01:38:26 +0600

    first updates

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SITE_NAME', "Cut your URL");
+define('HOST', "http://localhost/PHP-GLO");
+define('DB_HOST', "127.0.0.1");
+
+define('DB_NAME', "cut_url");
+define('DB_USER', "ROOT");
+define('DB_PASS', "");
+?>
